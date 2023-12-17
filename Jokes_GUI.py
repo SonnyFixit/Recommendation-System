7c01d12f930445664857aeecb4241724773b8f24
SonnyFixit <fikser@tlen.pl>
2023-12-17 22:14:28 +0100

    [Scripts] Improved SQLInsertion and GUI
    - SQLInsertion code got separated into more methods
    - Additonal functions for GUI - updating status of database, overall look and initialization of buttons

--- Jokes_GUI.py
+++ Jokes_GUI.py
@@ -1,52 +1,82 @@
 import tkinter as tk
+from tkinter import PhotoImage, font as tkFont
+from pathlib import Path
 
 from Jokes_SQLInsertion import main as main_sql_insertion
 from Jokes_DataPreprocessing import main as main_data_preprocessing
 from Jokes_UserCollaborativeFiltering import main as main_user_collaborative_filtering
-from pathlib import Path
-from tkinter import PhotoImage
-
-def check_database():
-    db_path = Path("Data/jester_jokes.db")
-    if db_path.is_file():
-        return "Database found.", "Icons/Status_Okay.png", True
-    else:
-        return "Database not found.", "Icons/Status_Error.png", False
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Jokes Recommendation System")
+        self.initialize_gui_elements()
+        self.update_database_status()
 
-        db_status_text, icon_path, db_exists = check_database()
-        self.db_status = tk.Label(self, text=db_status_text)
-        self.db_status.pack()
-
-        self.status_icon = PhotoImage(file=icon_path)
-        self.icon_label = tk.Label(self, image=self.status_icon)
-        self.icon_label.pack()
-
+    def initialize_gui_elements(self):
         self.result_area = tk.Text(self, height=30, width=100)
         self.result_area.pack()
 
-        if db_exists:
-            tk.Button(self, text="Run User Collaborative Filtering", command=self.execute_user_collaborative_filtering).pack()
+        buttonFont = tkFont.Font(family="Helvetica", size=12, weight="bold")  # Customize font
+
+        # Initialize both buttons, but they will be shown/hidden based on DB status
+        self.combined_button = tk.Button(self, text="Run SQL Insertion and Data Preprocessing", 
+                                         command=self.execute_sql_insertion_and_data_preprocessing_combined, 
+                                         bg="green", fg="white", font=buttonFont, padx=10, pady=5)
+        self.collaborative_filtering_button = tk.Button(self, text="Run User Collaborative Filtering", 
+                                                        command=self.execute_user_collaborative_filtering, 
+                                                        bg="blue", fg="white", font=buttonFont, padx=10, pady=5)
+
+        # Initially pack both buttons, their visibility will be updated later
+        self.combined_button.pack(pady=5)
+        self.collaborative_filtering_button.pack(pady=5)
+
+    def update_database_status(self):
+        db_status_text, icon_path, db_exists = self.check_database()
+
+        # Load the image and keep a reference to prevent garbage collection
+        self.status_image = PhotoImage(file=icon_path)
+
+        if hasattr(self, 'db_status'):
+            self.db_status.config(text=db_status_text)
+            self.icon_label.config(image=self.status_image)
         else:
-            tk.Button(self, text="Run SQL Insertion and Data Preprocessing", command=self.execute_sql_insertion_and_data_preprocessing_combined).pack()
+            self.db_status = tk.Label(self, text=db_status_text)
+            self.db_status.pack()
+            self.icon_label = tk.Label(self, image=self.status_image)
+            self.icon_label.pack()
+
+        # Update button visibility based on database status
+        if db_exists:
+            self.collaborative_filtering_button.pack(pady=5)
+            self.combined_button.pack_forget()
+        else:
+            self.combined_button.pack(pady=5)
+            self.collaborative_filtering_button.pack_forget()
+
+    @staticmethod
+    def check_database():
+        db_path = Path("Data/jester_jokes.db")
+        if db_path.is_file():
+            return f"Database found at: \n{db_path.absolute()}.", "Icons/Status_Okay.png", True
+        else:
+            return "Database not found.", "Icons/Status_Error.png", False
 
     def execute_user_collaborative_filtering(self):
-
         result = main_user_collaborative_filtering()
         self.result_area.insert(tk.END, result + "\n")
 
     def execute_sql_insertion_and_data_preprocessing_combined(self):
 
-            sql_insertion_result = main_sql_insertion()
-            self.result_area.insert(tk.END, sql_insertion_result + "\n")
+            self.result_area.insert(tk.END, "Starting SQL Insertion...\n")
+            main_sql_insertion()
+            self.result_area.insert(tk.END, "SQL Insertion completed.\n\n")
 
+            self.result_area.insert(tk.END, "Starting Data Preprocessing...\n")
+            main_data_preprocessing()
+            self.result_area.insert(tk.END, "Data Preprocessing completed.\n\n")
 
-            data_preprocessing_result = main_data_preprocessing()
-            self.result_area.insert(tk.END, data_preprocessing_result + "\n")
+            self.update_database_status()
 
 if __name__ == "__main__":
     app = App()
--- Jokes_SQLInsertion.py
+++ Jokes_SQLInsertion.py
@@ -1,85 +1,75 @@
 import sqlite3 as db
-import re
 import os
 import codecs
 import pandas as pd
 import html2text
 import numpy as np
 
-# Connect to the database (or create it if it doesn't exist)
-sql_db = 'jester_jokes'
-db_path = 'data/{}.db'.format(sql_db)
-conn = db.connect(db_path)
+# Connect to the database
+def connect_to_database(sql_db):
+    db_path = 'data/{}.db'.format(sql_db)
+    conn = db.connect(db_path)
+    return conn
 
 # Create a 'cursor' to execute commands
-c = conn.cursor()
-
 # For each user, only these 5 tables will be created, in which all data will be collected.
-c.execute("CREATE TABLE IF NOT EXISTS jokes (joke_id INTEGER, joke TEXT)")
-
-schema = "CREATE TABLE IF NOT EXISTS ratings(user_id Integer, number_of_jokes_rated Integer,"
-for i in range(1, 101):
-    schema = schema + 'joke_{} FLOAT(2), '.format(str(i))
-schema = schema[:-2] + ')'
-c.execute(schema)
-
-file = 'init1.html'
-data = codecs.open('data/raw/jokes/' + file, 'r', encoding="cp1252")
-joke_html = data.read()
-
+def create_tables(cursor):
+    cursor.execute("CREATE TABLE IF NOT EXISTS jokes (joke_id INTEGER, joke TEXT)")
+    
+    schema = "CREATE TABLE IF NOT EXISTS ratings(user_id Integer, number_of_jokes_rated Integer,"
+    for i in range(1, 101):
+        schema += 'joke_{} FLOAT(2), '.format(i)
+    schema = schema[:-2] + ')'
+    cursor.execute(schema)
+    
 # Extract the joke from the HTML file
-joke = html2text.html2text(joke_html)
-# Extract the joke's identifier
-joke_id = int(file.split('init')[1].split('.html')[0])
-
-print(joke_id)
-print(joke)
-
-# This method takes the location of the HTML file as input and returns the joke from it
-def joke_extractor(file):
-    data = codecs.open(file, 'r', encoding="cp1252")
-    joke_html = data.read()
+def extract_joke_from_html(file):
+    with codecs.open(file, 'r', encoding="cp1252") as data:
+        joke_html = data.read()
     joke = html2text.html2text(joke_html)
     joke_id = int(file.split('init')[1].split('.html')[0])
-    return (joke_id, joke)
+    return joke_id, joke
 
-files = os.listdir('data/raw/jokes')
-jokes_list = [joke_extractor('data/raw/jokes/'+ file) for file in files if file.endswith('.html')]
+#Saving jokes into database
+def insert_jokes_to_database(cursor):
+    files = os.listdir('data/raw/jokes')
+    jokes_list = [extract_joke_from_html('data/raw/jokes/' + file) for file in files if file.endswith('.html')]
+    cursor.executemany('INSERT INTO jokes VALUES (?,?)', jokes_list)
 
-c.executemany('INSERT INTO jokes VALUES (?,?)', jokes_list)
+# Saving ratings to database
+def insert_ratings_to_database(cursor, dataframe):
+    query = 'INSERT INTO ratings VALUES(' + ','.join(['?' for _ in dataframe.columns]) + ')'
+    cursor.executemany(query, dataframe.values)
 
-conn.commit()
-
-def insert_ratings_to_database(c, dataframe):
-
-    query = 'INSERT INTO ratings VALUES(' + ','.join(['?' for _ in range(len(dataframe.columns))]) + ')'
-    c.executemany(query, dataframe.values)
-
-def main():
-    
-    # Reading data from Excel files
+ # Reading data from Excel files
+def read_rating_data():
     df1 = pd.read_excel('data/raw/jester-data-1.xls', header=None)
     df2 = pd.read_excel('data/raw/jester-data-2.xls', header=None)
     df3 = pd.read_excel('data/raw/jester-data-3.xls', header=None)
+    return pd.concat([df1, df2, df3], axis=0)
+
+def main():
     
-    dataframe = pd.concat([df1, df2, df3], axis=0)
+    conn = connect_to_database('jester_jokes')
+    # Create a 'cursor' to execute commands
+    c = conn.cursor()
+
+    create_tables(c)
+    insert_jokes_to_database(c)
+    dataframe = read_rating_data()
     
     user_id = np.linspace(1, len(dataframe), len(dataframe))
     dataframe.insert(0, 'user_id', user_id)
-
-    # Saving the ratings to the database
-    insert_ratings_to_database(c, dataframe)
     
-    # Saving the changes to the database
+    insert_ratings_to_database(c, dataframe)
     conn.commit()
 
-    # Displaying the last few rows of the 'ratings' table (for verification)
     query = 'SELECT * FROM ratings'
     df = pd.read_sql(query, conn)
     print(df.tail())
-
-    # Saving data to a CSV file (just in case)
     df.to_csv('data/jester_jokes_rating.csv', index=None)
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
